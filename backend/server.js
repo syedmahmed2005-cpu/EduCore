e4f3e6a669e268040bf2175eaeb376d95f94d956
Syed Ahmed <syedmahmed2005@gmail.com>
2026-09-13 20:01:21 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  process.env.FRONTEND_URL,
+  "https://www.myeducore.app",
+  "https://myeducore.app",
+  "https://student-manegement-system-full-stac.vercel.app",
+   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
 app.use(
